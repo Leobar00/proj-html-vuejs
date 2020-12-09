5a552a6073ae6be934c11bf24f1609455569df25
Leonardo <barbierileon@gmail.com>
2020-12-09 15:36:37 +0100

    header completo

---
+++ js/function.js
@@ -0,0 +1,8 @@
+const link = [
+    'Home <i class="fa fa-angle-down" aria-hidden="true"></i>',
+    'Order Online',
+    'About',
+    'News',
+    'Contact Us',
+    '<i class="fa fa-shopping-cart" aria-hidden="true"></i>'
+]
---
+++ js/main.js
@@ -0,0 +1,7 @@
+const app = new Vue({
+    el: '#root',
+    data:{
+        numberHeaderLink: 6,
+        headerLink: [...link]
+    }
+})
